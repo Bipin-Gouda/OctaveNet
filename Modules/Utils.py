b1279fe604f90ce629b53dcb44dedd34b06d53f9
Bipin Gouda <bipingouda1199@gmail.com>
2024-07-15 06:53:24 +0530

    Updated (commented) Utils.py

--- Modules/Utils.py
+++ Modules/Utils.py
@@ -10,7 +10,7 @@ from Modules.Model import OctaveNet
 
 def PrepareLibrariesAndDatasets():
   print('Installing Libraries...', end=' ')
-  os.system("pip install torch==1.11.0+cu113 torchvision==0.12.0+cu113 torchaudio==0.11.0 --extra-index-url https://download.pytorch.org/whl/cu113")
+  #os.system("pip install torch==1.11.0+cu113 torchvision==0.12.0+cu113 torchaudio==0.11.0 --extra-index-url https://download.pytorch.org/whl/cu113")
   os.system("apt update")
   os.system("apt install aria2")
   print('Done.')
